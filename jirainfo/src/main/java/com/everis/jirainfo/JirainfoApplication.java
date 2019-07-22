1bd29db1690e21e95726df2cc3a40572f0b95c13
fabio <fabio@noteubuntu>
2019-07-22 20:41:38 -0300

    commit

---
+++ jirainfo/src/main/java/com/everis/jirainfo/JirainfoApplication.java
@@ -0,0 +1,13 @@
+package com.everis.jirainfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JirainfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JirainfoApplication.class, args);
+	}
+
+}
