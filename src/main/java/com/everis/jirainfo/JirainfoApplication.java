865c3034324ee70917e5c160e982f21b1d70b6d4
fabio <fabio@noteubuntu>
2019-07-24 23:29:44 -0300

    new web

--- src/main/java/com/everis/jirainfo/JirainfoApplication.java
+++ src/main/java/com/everis/jirainfo/JirainfoApplication.java
@@ -2,15 +2,13 @@ package com.everis.jirainfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 //@EntityScan(basePackages = { "com.everis.jirainfo.entity" })
 //@EnableJpaRepositories(basePackages = { "com.everis.jirainfo.repository" })
 //@ComponentScan(basePackages = {"com.everis.jirainfo.controller"})
-public class JirainfoApplication {
+public class JirainfoApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(JirainfoApplication.class, args);
 	}
